fa2ccb32335dbeadbb4688af2cd1622b6cad2574
illelI <ilellixd@gmail.com>
2023-05-02 15:18:09 +0200

    initial commit

---
+++ src/main/java/com/neteye/NetEyeApplication.java
@@ -0,0 +1,13 @@
+package com.neteye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetEyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetEyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neteye/NetEyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neteye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetEyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
